e4cb2dd9508ad06e791275d39e6a4d5b6f40f382
colin jaworski <colinrjaworski.84@gmail.com>
2022-05-18 09:43:18 -0500

    initial commit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,25 +2,85 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [creatureList, setCreatureList] = useState([
-    { name: 'Unicorn', origin: 'Britain' },
-    { name: 'Sphinx', origin: 'Egypt' },
-    { name: 'Jackalope', origin: 'America' }
-  ])
+
+  const [result, setResult] = useState('');
+  const [userChoice, setUserChoice] = useState('');
+
+  const getUserChoice = (userChoice) => {
+    userChoice = userChoice.toLowerCase();
+    if (userChoice === 'rock' || userChoice === 'paper' || userChoice === 'scissors') {
+      return userChoice
+    } else {
+      // console.log(`Sorry, ${userChoice} is not valid `);
+    }
+  }
+
+  const getComputerChoice = () => {
+    let computerChoice = Math.floor(Math.random() * 3);
+    switch (computerChoice) {
+      case 0:
+        return 'rock';
+      case 1:
+        return 'paper';
+      case 2:
+        return 'scissors';
+    }
+  }
+
+  const determineWinner = (userChoice, computerChoice) => {
+
+    if (userChoice === computerChoice) {
+      setResult('This game was a tie');
+    }
+
+    if (userChoice === 'rock') {
+      if (computerChoice === 'paper') {
+        setResult('The computer won');
+      } else {
+        setResult('You won')
+      }
+    }
+
+    if (userChoice === 'paper') {
+      if (computerChoice === 'rock') {
+        setResult('You won');
+      } else {
+        setResult('The computer won');
+      }
+    }
+
+    if (userChoice === 'scissors') {
+      if (computerChoice === 'rock') {
+        setResult('You lost bud');
+      } else {
+        setResult('Yay, you win!');
+      }
+    }
+  }
+
+  function playGame() {
+
+  }
+
+  const setChoice = (value) => {
+    setUserChoice(value)
+    console.log(value);
+  }
 
   return (
     <>
       <header className="Header">
-        <h1>React Mythical Creatures</h1>
+        <h1>Rock Paper Scissors</h1>
       </header>
-      <div className="App">
-        <div>
-          Splatting to the DOM looks like this:
-          <pre>
-            {/* We can use JSON.stringify() to dump raw data into our DOM */}
-            {JSON.stringify(creatureList)}
-          </pre>
-        </div>
+      <div>
+
+        <input type="text" onChange={(event)=>{setChoice(event.target.value)}} />
+
+
+
+
+        <button onClick={playGame}>Play Game?</button>
+        <h1>{result}</h1>
       </div>
     </>
   );
